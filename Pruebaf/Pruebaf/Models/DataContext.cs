b259e15ee6100235c3c6784ad9a4874ef683691b
sergio hoyos <yeyo_hoyos@hotmail.com>
2020-01-03 01:15:46 -0400

    entityinstalado

---
+++ Pruebaf/Pruebaf/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Pruebaf.Models
+{
+    public class DataContext:DbContext
+    {
+    }
+}
---
+++ Pruebaf/Pruebaf/Models/producto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pruebaf.Models
+{
+    public class producto
+    {
+    }
+}
